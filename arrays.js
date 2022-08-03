00ce3de08396df195680b68c28cae048b6c8104a
Jocelle Bautista <jcllebautista@gmail.com>
2022-08-03 18:39:32 -0400

    Call for arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,6 @@
 var strings = ["stringA", "stringB", "stringC", "stringB"]
 var numbers = [0, 1, -2, 3]
 var booleans = [true , false , true , true]
+strings.pop()
+numbers.shift()
+booleans.slice(1)
