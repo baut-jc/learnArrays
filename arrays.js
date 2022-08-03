ac29cc18363e705fc701b3f4008812bc4dc0c73f
Jocelle Bautista <jcllebautista@gmail.com>
2022-08-03 18:51:27 -0400

    Explain methods

--- arrays.js
+++ arrays.js
@@ -1,6 +1,13 @@
-var strings = ["stringA", "stringB", "stringC", "stringB"]
+var strings = ["stringA", "stringB", "stringC", "stringD"]
 var numbers = [0, 1, -2, 3]
 var booleans = [true , false , true , true]
 strings.pop()
+console.log(strings)
 numbers.shift()
-booleans.slice(1)
+console.log(numbers)
+booleans.unshift(false)
+console.log(booleans)
+
+//pop will remove the last element from the array
+//shift will remove the first element of the array
+//unshift will add as the first element of an array
